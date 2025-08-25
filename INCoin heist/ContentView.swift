472d4eec20441a877d04cad51b88502feb9ae909
terrablade12 <terrablade1212@gmail.com>
2025-08-25 16:54:17 +0800

    Initial Commit

---
+++ INCoin heist/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  INCoin heist
+//
+//  Created by Iain Tan Qi yuan on 25/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ INCoin heist/INCoin_heistApp.swift
@@ -0,0 +1,17 @@
+//
+//  INCoin_heistApp.swift
+//  INCoin heist
+//
+//  Created by Iain Tan Qi yuan on 25/8/25.
+//
+
+import SwiftUI
+
+@main
+struct INCoin_heistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
